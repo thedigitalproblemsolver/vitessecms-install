9d722d9cb6a266c4bee739e8622595a5edf51ace
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-18 22:44:00 +0100

    Split adminmenu builders to module listeners

---
+++ src/Listeners/AdminMenuListener.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Install\Listeners;
+
+use VitesseCms\Admin\Models\AdminMenu;
+use VitesseCms\Admin\Models\AdminMenuNavBarChildren;
+use Phalcon\Events\Event;
+
+class AdminMenuListener
+{
+    public function AddChildren(Event $event, AdminMenu $adminMenu): void
+    {
+        if ($adminMenu->getUser()->getPermissionRole() === 'superadmin') :
+            $children = new AdminMenuNavBarChildren();
+            $children->addChild('Site Creator', 'admin/install/sitecreator/index');
+
+            $adminMenu->addDropdown('System',$children);
+        endif;
+    }
+}
