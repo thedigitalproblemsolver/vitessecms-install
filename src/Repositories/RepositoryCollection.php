4794eeedaa47216f383f1f8a506e985374065914
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-01-10 21:22:38 +0100

    Phalcon 3 > 5 changes

--- src/Repositories/RepositoryCollection.php
+++ src/Repositories/RepositoryCollection.php
@@ -2,10 +2,7 @@
 
 namespace VitesseCms\Install\Repositories;
 
-use VitesseCms\Content\Repositories\ItemRepository;
 use VitesseCms\Database\Interfaces\BaseRepositoriesInterface;
-use VitesseCms\Datafield\Repositories\DatafieldRepository;
-use VitesseCms\Datagroup\Repositories\DatagroupRepository;
 use VitesseCms\User\Repositories\PermissionRoleRepository;
 
 class RepositoryCollection implements RepositoryCollectionInterface, BaseRepositoriesInterface
